970121e477e81c94de0eb9d447b7e43542e19d02
RodrigodO <45038483+RodrigodO@users.noreply.github.com>
2019-05-01 15:04:17 +0200

    Add files via upload

---
+++ app/src/main/java/com/ucl/LSINF1225/Themes.java
@@ -0,0 +1,13 @@
+package com.ucl.LSINF1225;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Themes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_themes);
+    }
+}
