5a0af9dd59fb6dd3e5cdddf3acf1d6b6f67e5353
Rodrigo <digo.do12@gmail.com>
2019-05-02 18:06:19 +0200

    Buttons

--- app/src/main/java/com/ucl/LSINF1225/Themes.java
+++ app/src/main/java/com/ucl/LSINF1225/Themes.java
@@ -2,11 +2,21 @@ package com.ucl.LSINF1225;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Themes extends AppCompatActivity {
 
+    private Button StartButton;
+    private RadioGroup ButtonGroup;
+    private RadioButton ButtonMath, ButtonBio, ButtonPhys, ButtonChe, ButtonLog, ButtonCul, ButtonHis, ButtonOth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themes);
     }
