da0542487493428197410e0e74b6b60f9a23a12e
Rodrigo <digo.do12@gmail.com>
2019-05-06 11:32:51 +0200

    Update Switch Thèmes

--- app/src/main/java/com/ucl/LSINF1225/Themes.java
+++ app/src/main/java/com/ucl/LSINF1225/Themes.java
@@ -1,8 +1,11 @@
 package com.ucl.LSINF1225;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -15,9 +18,41 @@ public class Themes extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        StartButton = (Button)findViewById(R.id.StartButton);
+        ButtonGroup = (RadioGroup)findViewById(R.id.ButtonGroup);
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themes);
+    }
+    public void buttonclick (View view){
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.ButtonMath:
+                if (checked)
+                    break;
+            case R.id.ButtonBio:
+                if (checked)
+                    break;
+            case R.id.ButtonPhys:
+                if (checked)
+                    break;
+            case R.id.ButtonChe:
+                if (checked)
+                    break;
+            case R.id.ButtonLog:
+                if (checked)
+                    break;
+            case R.id.ButtonCul:
+                if (checked)
+                    break;
+            case R.id.ButtonHist:
+                if (checked)
+                    break;
+            case R.id.ButtonOth:
+                if (checked)
+                    break;
+        }
+
     }
 }
