6338b8e668245142c18b0166ddc97b0b1c7f5b26
Rodrigo <digo.do12@gmail.com>
2019-05-06 17:26:46 +0200

    Ecran Resultat

---
+++ app/src/main/java/com/ucl/LSINF1225/resultat.java
@@ -0,0 +1,26 @@
+package com.ucl.LSINF1225;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class resultat extends AppCompatActivity {
+
+    private TextView TextName, TextQI;
+    private Button delete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultat);
+
+        TextName = (TextView)findViewById(R.id.TextName);
+        TextQI = (TextView)findViewById(R.id.TextQI);
+        delete = (Button)findViewById(R.id.delete);
+
+        // TextView t1 = (TextView) findViewById(R.id.textView1);
+        // t1.setText("RRRRRR");
+
+    }
+}
